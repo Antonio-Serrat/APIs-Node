e13e0f8c2c5af33dfe196c7b6d7b35f753fd53be
ToniSrt <4antoniogabriel20@gmail.com>
2022-02-16 22:49:57 -0300

    update

--- App-Ejs/index.js
+++ App-Ejs/index.js
@@ -2,19 +2,26 @@ const express = require('express')
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT | 8080
-const productsRouter = require("./routes/products")
-const homeRouter = require('./routes/home');
 
+const homeRouter = require('./routes/home');
+const ejsEngine = require('./engines/ejss')
+
+//TEMPLATES
+ejsEngine(app)
+
+//MIDDLEWARES
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+//STATIC FILES
 app.use("/static", express.static(path.join(__dirname, 'public')))
 
-app.use("/api/productos", productsRouter);
+//ROUTERS
 app.use("/", homeRouter);
 
 
 
+//CONNECTION
 const server = app.listen(PORT, () => {
     console.log(`escuchando en el puerto ${PORT}`)
 })
